05e05a87085411c90dae9cc13f30dd1d400d621a
Dieuveille MPOUNKOUO <94381407+DieuveilleSuthi@users.noreply.github.com>
2024-03-30 20:56:04 +0100

    2024-03-30-HomePage

--- frontend/src/pages/HomeScreen/HomeScreen.js
+++ frontend/src/pages/HomeScreen/HomeScreen.js
@@ -1,13 +1,30 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { View, Image, Text, Pressable } from 'react-native';
 
-import styles from './HomeScreenCss.js'
+import styles from './HomeScreenCss.js';
 
 const HomeScreen = () => {
     return (
-        <View style= {styles.container}>
+        <View style={styles.container}>
             <View>
-                <Text style={styles.textItem}>HomeScreen</Text>
+                <Image source={{ uri: "https://www.thelinkottawa.ca/en/eat-right-be-active/resources/Images/running-1065.jpg" }} style={styles.image} />
+                <View style={styles.textContainer}>
+                    <Text style={styles.textItem}>Next activity</Text>
+                    <Text style={styles.textItem2}> Aujourd'hui à 12h</Text>
+                </View>
+                <View style={styles.ButtonContainer}>
+                    <Pressable style={styles.btnName}>
+                        <Text>Nom de l'activité</Text>
+                    </Pressable>
+                </View>
+            </View>
+            <View style={styles.TextPresentation}>
+                <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac tortor vel risus dictum euismod. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nullam ut nisi euismod, pulvinar odio nec, aliquet arcu. Sed ac ex eu magna fermentum euismod vel a felis. Proin nec neque quis risus placerat laoreet. Nullam non mauris nec nisi sodales fermentum vel nec tellus. Vivamus sagittis tortor eget dolor vehicula, in mollis nulla feugiat. Ut nec urna a libero elementum blandit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed eget gravida justo, sit amet euismod sapien. Donec id ante quis elit cursus interdum. Suspendisse lacinia ante nunc, et maximus libero varius sed. Vivamus id turpis id velit bibendum sodales non eget ex. Cras ultricies, lacus quis lobortis rhoncus, justo elit venenatis felis, in fringilla mauris odio et justo. Sed fringilla, felis eget tristique congue, nibh ipsum gravida tortor, nec varius risus risus vel dui. </Text>
+            </View>
+            <View>
+                <Pressable style={styles.btnCreate}>
+                    <Text> Create or Modify your planning</Text>
+                </Pressable>
             </View>
         </View>
     )
--- frontend/src/pages/HomeScreen/HomeScreenCss.js
+++ frontend/src/pages/HomeScreen/HomeScreenCss.js
@@ -1,4 +1,5 @@
 import { StyleSheet } from "react-native";
+import { endAsyncEvent } from "react-native/Libraries/Performance/Systrace";
 
 export default StyleSheet.create(
     {
@@ -8,7 +9,53 @@ export default StyleSheet.create(
         },
         textItem: {
             textAlign: 'center',
-            color: "white",
+            color: "#282626",
+            fontSize: 20,
+            fontWeight: 'bold',
+            marginLeft: 15
+        },
+        textItem2: {
+            textAlign: 'center',
+            color: "#282626",
+            marginLeft: 15
+        },
+        image: {
+            width: 350,
+            height: 200,
+            marginLeft: 20,
+            borderRadius: 10
+        },
+        textContainer: {
+            position: 'absolute',
+            top: 10,
+            left: 20
+        },
+        ButtonContainer: {
+            position: 'absolute',
+            top: 150,
+            marginLeft: 210
+        },
+        btnName: {
+            borderRadius: 10,
+            padding: 10,
+            backgroundColor: '#87CEEB'
+        },
+        TextPresentation: {
+            padding: 10,
+            backgroundColor: '#87CEEB',
+            marginLeft: 20,
+            borderRadius: 10,
+            marginRight: 20,
+            marginVertical: 20,
+        },
+        btnCreate: {
+            borderRadius: 10,
+            padding: 10,
+            backgroundColor: '#87CEEB',
+            alignItems: 'center',
+            marginRight: 20,
+            marginLeft: 20,
         }
+        
     }
 )
